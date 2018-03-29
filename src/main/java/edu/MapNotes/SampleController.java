66969cddbe242184501025e0bf412d6f3f33cc05
Radomir Krawczykiewicz <kotwic4@gmail.com>
2018-03-29 21:28:24 +0200

    Sample Controller

---
+++ src/main/java/edu/MapNotes/SampleController.java
@@ -0,0 +1,13 @@
+package edu.MapNotes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping
+    public String sampleApi(){
+        return "Hello World";
+    }
+}

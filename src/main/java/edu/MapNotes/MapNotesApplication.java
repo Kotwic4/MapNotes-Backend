95cdd91b53c2613341223ca9c16d903dfc5fa88b
Radomir Krawczykiewicz <kotwic4@gmail.com>
2018-03-29 19:58:29 +0200

    Sample Spring Boot project

---
+++ src/main/java/edu/MapNotes/MapNotesApplication.java
@@ -0,0 +1,12 @@
+package edu.MapNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapNotesApplication.class, args);
+	}
+}
